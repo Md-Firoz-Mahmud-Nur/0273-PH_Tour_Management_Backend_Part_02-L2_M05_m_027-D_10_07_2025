3b4b6b6e5604b2a41453ff156ba56ac04956e5c9
firoz.nur <firoznur5@gmail.com>
2025-07-26 21:33:08 +0600

    Add : Validate Request Middleware

---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,36 @@
+import z from "zod";
+
+export const createZodSchema = z.object({
+  name: z
+    .string({ invalid_type_error: "Name must be string" })
+    .min(3, { message: "Name too short, enter at least 3 character" })
+    .max(50, { message: "Name too long, enter at most 50 character" }),
+  email: z
+    .string()
+    .email({ message: "Enter a valid email" })
+    .min(5, { message: "Email must be at least 5 characters long." })
+    .max(100, { message: "Email cannot exceed 100 characters." }),
+  password: z
+    .string({ invalid_type_error: "Password must be string" })
+    .min(8, { message: "Password must be at least 8 characters long." })
+    .regex(/^(?=.*[A-Z])/, {
+      message: "Password must contain at least 1 uppercase letter.",
+    })
+    .regex(/^(?=.*[!@#$%^&*])/, {
+      message: "Password must contain at least 1 special character.",
+    })
+    .regex(/^(?=.*\d)/, {
+      message: "Password must contain at least 1 number.",
+    }),
+  phone: z
+    .string({ invalid_type_error: "Phone Number must be string" })
+    .regex(/^(?:\+8801\d{9}|01\d{9})$/, {
+      message:
+        "Phone number must be valid for Bangladesh. Format: +8801XXXXXXXXX or 01XXXXXXXXX",
+    })
+    .optional(),
+  address: z
+    .string({ invalid_type_error: "Address must be string" })
+    .max(200, { message: "Address cannot exceed 200 characters." })
+    .optional(),
+});
