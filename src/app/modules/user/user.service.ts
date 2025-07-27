705fb5d44b6a8211a9b0f84425fced377d2c9042
firoz.nur <firoznur5@gmail.com>
2025-07-27 11:37:42 +0600

    Add : Hashed Password

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,15 +2,18 @@ import AppError from "../../errorHelpers/AppError";
 import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
 import httpStatus from "http-status-codes";
+import bcrypt from "bcryptjs";
 
 const createUser = async (payload: Partial<IUser>) => {
-  const { email, ...rest } = payload;
+  const { email, password, ...rest } = payload;
 
   const isUserExist = await User.findOne({ email });
 
   if (isUserExist) {
     throw new AppError(httpStatus.BAD_REQUEST, "User already exist");
   }
+
+  const hashedPassword = await bcrypt.hash(password as string, 10);
 
   const authProvider: IAuthProvider = {
     provider: "credentials",
@@ -19,6 +22,7 @@ const createUser = async (payload: Partial<IUser>) => {
 
   const user = await User.create({
     email,
+    password: hashedPassword,
     auths: [authProvider],
     ...rest,
   });
