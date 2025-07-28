ddb25d30df72f373d85d461c16ad32d4aa2160cb
firoz.nur <firoznur5@gmail.com>
2025-07-28 23:46:48 +0600

    Add : User Type Verification

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,7 +1,10 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { userControllers } from "./user.controller";
 import { createZodSchema } from "./user.validation";
 import { validateRequest } from "../../middlewares/validateRequest";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import AppError from "../../errorHelpers/AppError";
+import { Role } from "./user.interface";
 
 const router = Router();
 
@@ -10,6 +13,31 @@ router.post(
   validateRequest(createZodSchema),
   userControllers.createUser
 );
-router.get("/all-users", userControllers.getAllUsers);
+router.get(
+  "/all-users",
+
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const accessToken = req.headers.authorization;
+      if (!accessToken) {
+        throw new AppError(403, "Access token not found");
+      }
+      const verifyAccessToken = jwt.verify(accessToken as string, "secret");
+
+      if (
+        (verifyAccessToken as JwtPayload).role !== Role.ADMIN &&
+        (verifyAccessToken as JwtPayload).role !== Role.SUPER_ADMIN
+      ) {
+        throw new AppError(403, "You are not an admin");
+      }
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  userControllers.getAllUsers
+);
 
 export const userRoutes = router;
